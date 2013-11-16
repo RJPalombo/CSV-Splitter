8d969f49023327011a59554633fc8668bd2b88cf
RJ Palombo <rj@rjpalombo.com>
2013-11-16 12:51:39 -0500

    Initial Commit

---
+++ CSV Splitter/CSVSplitter.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace CSV_Splitter
+{
+    partial class CSVSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CSVSplitter));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.close = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.fileToSplit = new System.Windows.Forms.TextBox();
+            this.saveLocation = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.recordsToSplitLabel = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.recordsToSplit = new System.Windows.Forms.TextBox();
+            this.firstRowIsHeader = new System.Windows.Forms.CheckBox();
+            this.copyHeaders = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "csv";
+            this.openFileDialog1.Filter = "Coma Separated Values|*.csv";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(275, 202);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 23);
+            this.close.TabIndex = 0;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(196, 202);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 1;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File to Split";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Choose the location for your files";
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users";
+            // 
+            // fileToSplit
+            // 
+            this.fileToSplit.Location = new System.Drawing.Point(129, 65);
+            this.fileToSplit.Name = "fileToSplit";
+            this.fileToSplit.Size = new System.Drawing.Size(221, 20);
+            this.fileToSplit.TabIndex = 3;
+            this.fileToSplit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.fileToSplit_MouseClick);
+            // 
+            // saveLocation
+            // 
+            this.saveLocation.Location = new System.Drawing.Point(129, 91);
+            this.saveLocation.Name = "saveLocation";
+            this.saveLocation.Size = new System.Drawing.Size(221, 20);
+            this.saveLocation.TabIndex = 5;
+            this.saveLocation.MouseClick += new System.Windows.Forms.MouseEventHandler(this.saveLocation_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Location for Split files";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 53);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "This program is intedend to split one CSV into many CSV files for a given number " +
+                "you specify. You can also choose the location for your saved files.";
+            // 
+            // recordsToSplitLabel
+            // 
+            this.recordsToSplitLabel.AutoSize = true;
+            this.recordsToSplitLabel.Location = new System.Drawing.Point(12, 122);
+            this.recordsToSplitLabel.Name = "recordsToSplitLabel";
+            this.recordsToSplitLabel.Size = new System.Drawing.Size(82, 13);
+            this.recordsToSplitLabel.TabIndex = 7;
+            this.recordsToSplitLabel.Text = "Records to Split";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 237);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(378, 22);
+            this.statusStrip.TabIndex = 8;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // recordsToSplit
+            // 
+            this.recordsToSplit.Location = new System.Drawing.Point(129, 119);
+            this.recordsToSplit.Name = "recordsToSplit";
+            this.recordsToSplit.Size = new System.Drawing.Size(100, 20);
+            this.recordsToSplit.TabIndex = 9;
+            this.recordsToSplit.TextChanged += new System.EventHandler(this.recordsToSplit_TextChanged);
+            // 
+            // firstRowIsHeader
+            // 
+            this.firstRowIsHeader.AutoSize = true;
+            this.firstRowIsHeader.Checked = true;
+            this.firstRowIsHeader.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.firstRowIsHeader.Location = new System.Drawing.Point(15, 156);
+            this.firstRowIsHeader.Name = "firstRowIsHeader";
+            this.firstRowIsHeader.Size = new System.Drawing.Size(177, 17);
+            this.firstRowIsHeader.TabIndex = 10;
+            this.firstRowIsHeader.Text = "First row in Source file is Header";
+            this.firstRowIsHeader.UseVisualStyleBackColor = true;
+            this.firstRowIsHeader.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // copyHeaders
+            // 
+            this.copyHeaders.AutoSize = true;
+            this.copyHeaders.Location = new System.Drawing.Point(15, 179);
+            this.copyHeaders.Name = "copyHeaders";
+            this.copyHeaders.Size = new System.Drawing.Size(160, 17);
+            this.copyHeaders.TabIndex = 11;
+            this.copyHeaders.Text = "Copy Headers into Split Files";
+            this.copyHeaders.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 231);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(335, 30);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.Visible = false;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(309, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 9);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "by RJ Palombo";
+            // 
+            // CSVSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 259);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.copyHeaders);
+            this.Controls.Add(this.firstRowIsHeader);
+            this.Controls.Add(this.recordsToSplit);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.recordsToSplitLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.saveLocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileToSplit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CSVSplitter";
+            this.Text = "CSV Splitter";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox fileToSplit;
+        private System.Windows.Forms.TextBox saveLocation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label recordsToSplitLabel;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.TextBox recordsToSplit;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.CheckBox firstRowIsHeader;
+        private System.Windows.Forms.CheckBox copyHeaders;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CSV Splitter/CSVSplitter.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_Splitter
+{
+    public partial class CSVSplitter : Form
+    {
+        private int numberOfLines = 0;
+        private static string recordsToSplitDefault = "5000";
+
+        private int intRecordsToSplit;
+        //private int numberLeftOff;
+
+        //stores the file contents from the CSV file
+        private List<string> sFileContents = new List<string>();
+
+        //stores head from CSV if needed
+        private string sHeader;
+
+        //All Batch Data
+        Dictionary<int, List<string>> fileBatches = new Dictionary<int, List<string>>();
+ 
+        public CSVSplitter()
+        {
+            InitializeComponent();
+            //recordsToSplit default value
+            recordsToSplit.Text = recordsToSplitDefault;
+        }
+
+        private void updateNumberOfLinesAndStoreFileContents()
+        {
+            //Reset
+            numberOfLines = 0;
+            sHeader = null;
+            //Set intRecordsToSplit
+            intRecordsToSplit = int.Parse(recordsToSplit.Text);
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+
+            try
+            {
+                //Count Lines
+                while (!sr.EndOfStream)
+                {
+                    string srLine = sr.ReadLine();
+
+                    //Copy Header
+                    if (firstRowIsHeader.Checked && numberOfLines == 0 && sHeader == null)
+                    {
+                        sHeader = srLine;
+                    }
+                    else
+                    {
+                        sFileContents.Add(srLine);
+                        numberOfLines++;
+                    }   
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                                ex.Message.ToString() + Environment.NewLine +
+                                ex.Data + Environment.NewLine +
+                                ex.InnerException + Environment.NewLine +
+                                ex.Source + Environment.NewLine +
+                                ex.StackTrace
+                                );
+            }
+            finally
+            {
+                sr.Close();
+                UpdateStatus();
+            }
+        }
+
+        private int getRecordStartNumber(int batch)
+        {
+            int StartNum = (batch * intRecordsToSplit) - intRecordsToSplit;
+            return StartNum;
+        }
+
+        private int getRecordEndNumber(int batch)
+        {
+            int EndNum = getRecordStartNumber(batch) + intRecordsToSplit;
+            return EndNum;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            //Set intRecordsToSplit
+            intRecordsToSplit = int.Parse(recordsToSplit.Text);
+
+            for(int batchNum = 0; batchNum < numberOfBatchesToCreate(); batchNum++) 
+            {
+                int currentBatchNumber = batchNum + 1;
+                fillBatch(currentBatchNumber);
+            }
+
+            //now lets see the preview
+            int i = 1;
+            foreach (KeyValuePair<int, List<String>> entry in fileBatches)
+            {
+                List<string> tempFileContents = new List<string>();
+                foreach (string s in entry.Value) 
+                {
+                    //Add to temp file contents
+                    tempFileContents.Add(s);
+                    //For Preview Only
+                    listBox1.Items.Add(s);
+                }
+                //now create the csv file
+                createFile(i,tempFileContents);
+
+                i++;
+            }
+
+            Application.Exit();
+            
+        }
+
+        private void createFile(int batch, List<string> fileContents)
+        {
+            if (saveLocation.Text != "")
+            {
+                if (Directory.Exists(saveLocation.Text))
+                {
+                    string newPath = Path.Combine(saveLocation.Text, "Batch_" + batch + ".csv");
+                    //we will overwrite any file with this name so we will not need to check if it already exists
+                    using ( StreamWriter sw = new StreamWriter(newPath) )
+                    {
+                        if (copyHeaders.Checked) 
+                        { 
+                            sw.WriteLine(sHeader); 
+                        }
+                        foreach (string s in fileContents)
+                        {
+                            if (s != sHeader)
+                            {
+                                sw.WriteLine(s);
+                            }
+                        }
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The directory does not exist. Please create one and try again.");
+                }
+            }
+        }
+
+        private int numberOfBatchesToCreate()
+        {
+            if (recordsToSplit.Text == null || recordsToSplit.Text == "") { return 0; }
+
+            if (numberOfLines == 0) { return 0; }
+
+            if (int.Parse(recordsToSplit.Text.ToString()) > numberOfLines) { return 1; }
+
+            decimal numBatches = (decimal)numberOfLines / (decimal)intRecordsToSplit;
+
+            return (int)Math.Ceiling(numBatches);
+        }
+
+        private int getCurrentBatchNumber(int currentArrayNumber)
+        {
+            int batchNum = 0;
+            int numBatches = numberOfBatchesToCreate();
+
+            if (numBatches == 0) { return 0; }
+
+            for (int initBatchNumber = 1; initBatchNumber < numBatches; initBatchNumber++)
+            {
+                if (currentArrayNumber <= initBatchNumber * intRecordsToSplit)
+                {
+                    batchNum = initBatchNumber;
+                }
+            }
+            return batchNum;
+        }
+
+        public void fillBatch(int batchNumber) 
+        {
+            if (sFileContents.Count > 0)
+            {
+                try
+                {   
+                    //Read Lines of File Contents in batches
+
+                    List<string> tempList = new List<string>();
+                    int recStartNum = getRecordStartNumber(batchNumber);
+                    int recEndNum = getRecordEndNumber(batchNumber);
+                    if (recEndNum > numberOfLines) { recEndNum = numberOfLines; }
+                    for (int startNum = recStartNum; startNum < recEndNum; startNum++)
+                    {
+                        tempList.Add(sFileContents[startNum]);
+                    }
+
+                    //fill batch with temp list
+                    fileBatches.Add(batchNumber, tempList);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show( 
+                                    ex.Message.ToString() + Environment.NewLine + 
+                                    ex.Data + Environment.NewLine + 
+                                    ex.InnerException  + Environment.NewLine + 
+                                    ex.Source  + Environment.NewLine + 
+                                    ex.StackTrace
+                                    );
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (firstRowIsHeader.Checked)
+            {
+                copyHeaders.Enabled = true;
+            }
+            else
+            {
+                copyHeaders.Enabled = false;
+            }
+            if (fileToSplit.Text != "")
+            {
+                updateNumberOfLinesAndStoreFileContents();
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.Items.Count+" Items in the list");
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void UpdateStatus()
+        {
+            toolStripStatusLabel1.Text = "This app will create " + numberOfBatchesToCreate() + " CSV Files from a total of " + numberOfLines + " rows... ";
+        }
+
+        private void recordsToSplit_TextChanged(object sender, EventArgs e)
+        {
+            if (recordsToSplit.Text == null || recordsToSplit.Text == "") { recordsToSplit.Text = recordsToSplitDefault; }
+            UpdateStatus();
+        }
+
+        private void fileToSplit_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                updateNumberOfLinesAndStoreFileContents();
+            }
+        }
+
+        private void saveLocation_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                saveLocation.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            fileToSplit.Text = openFileDialog1.FileName.ToString();
+        }
+
+    }
+}
